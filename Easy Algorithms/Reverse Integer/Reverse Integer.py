93ca5a5c49d056faf35f03d337e311a65b9cd692
Allysia Jones <Allysia997@gmail.com>
2019-02-27 16:45:31 -0500

    Create Reverse Integer.py

---
+++ Easy Algorithms/Reverse Integer/Reverse Integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+            num = str(x)[1:]
+            result = int(num[::-1]) * -1 
+        else:
+            result = int(str(x)[::-1])
+        
+        if result >= 2 ** 31 or result <= -2 **31:
+            return 0
+        else:
+            return result
